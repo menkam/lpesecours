801391aa1f60f9ce4ae6995f1ff5667eb1e1dbe5
menkam <menkam35@gmail.com>
2019-03-21 12:51:51 +0100

    modif

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Tlist_groupe_user;
+use App\Models\Tlist_acreditation;
 
 class UserSeeder extends Seeder
 {
@@ -12,16 +14,33 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $groupeSystem = Tlist_groupe_user::where('code', 'SYSTE')->first();
+        $acreditationSystem = Tlist_acreditation::where('numero', '123456')->first();
+
+        $groupeVsiteur = Tlist_groupe_user::where('code', 'VSTER')->first();
+        $acreditationVsiteur = Tlist_acreditation::where('numero', '3')->first();
 
         $object = new User();
-        $object->groupe_user = '1';
-        $object->acreditation = '11';
-        $object->name = 'ADMIN';
+        $object->groupe_user = $groupeSystem['id'];
+        $object->acreditation = $acreditationSystem['id'];
+        $object->name = 'ROOT2';
         $object->surname = 'Admin';
-        $object->date_nais = '21-03-2019';
+        $object->date_nais = '21/03/2019';
         $object->sexe = 'M';
         $object->telephone = '+237670928110';
-        $object->email = 'menkam35@gmail.com';
+        $object->email = 'lpesecours@gmail.com';
+        $object->password = bcrypt('12345678');
+        $object->save();
+
+        $object = new User();
+        $object->groupe_user = $groupeVsiteur['id'];
+        $object->acreditation = $acreditationVsiteur['id'];
+        $object->name = 'VISITEUR';
+        $object->surname = 'invite';
+        $object->date_nais = '21/03/2019';
+        $object->sexe = 'M';
+        $object->telephone = '+237600000000';
+        $object->email = 'visiteur@gmail.com';
         $object->password = bcrypt('12345678');
         $object->save();
     }
