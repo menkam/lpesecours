e09da4f154624eb844d20c5f7d302e6020802bff
menkam <menkam35@gmail.com>
2019-04-01 14:36:44 +0100

    modif

---
+++ resources/views/Contacts.blade.php
@@ -0,0 +1,41 @@
+@extends("layouts.template")
+@section("title")
+    <title></title>
+    <meta name="description" content=" with some customizations as described in docs" />
+@endsection
+
+@section("style")
+
+@endsection
+
+@section("breadcrumb")
+    <ul class="breadcrumb">
+        <li>
+            <i class="ace-icon fa fa-home home-icon"></i>
+            <a href="/">Home</a>
+        </li>
+        <li class="active"><a href="contact">Cantact</a></li>
+    </ul>
+@endsection
+
+@section("page-header")
+    <h1>
+        Contact
+        <small>
+            <i class="ace-icon fa fa-angle-double-right"></i>
+            Common form elements and layouts
+        </small>
+    </h1>
+@endsection
+
+@section("content")
+    <div class="">Page contact.php en cours de developpement....</div>
+@endsection
+
+@section("scripts")
+
+@endsection
+
+@section("scripts2")
+
+@endsection
